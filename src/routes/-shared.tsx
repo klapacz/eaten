4966bb43c80af9e84be636062eea2e9e0613e530
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2025-10-13 22:04:13 +0200

    chore: calendar route

---
+++ src/routes/-shared.tsx
@@ -0,0 +1,239 @@
+import { Button } from '@/components/ui/button'
+import { mealCollection } from '@/db-collections'
+import { IconTrash } from '@intentui/icons'
+import z from 'zod'
+import { Sheet } from '@/components/ui/sheet'
+import {
+  TanstackForm,
+  useAppForm,
+  withFieldGroup,
+} from '@/integrations/tanstack-form'
+import {
+  CalendarDateTime,
+  getLocalTimeZone,
+  parseDateTime,
+  Time,
+  toCalendarDateTime,
+  today,
+} from '@internationalized/date'
+import { useMemo } from 'react'
+import { Select } from '@/components/ui/select'
+import { fieldStyles } from '@/components/ui/field'
+import { Separator } from '@/components/ui/separator'
+import {
+  Meal,
+  mealSchema,
+  mealTypeSchema,
+  mealTypeToDisplayText,
+} from '@/schemas/meal'
+
+export const validateSearch = z.union([
+  z.object({
+    meal: z.uuid().optional(),
+    add: z.undefined().optional(),
+  }),
+  z.object({
+    meal: z.undefined().optional(),
+    add: z.literal(true),
+  }),
+])
+
+export function CreateMealSheetContent({ close }: { close: () => void }) {
+  const defaultValues: z.infer<typeof mealFormSchema> = useMemo(
+    () => ({
+      id: crypto.randomUUID(),
+      datetime: toCalendarDateTime(today(getLocalTimeZone()), new Time(12, 0)),
+      type: 'BREAKFAST',
+      items: [],
+    }),
+    [],
+  )
+
+  const form = useAppForm({
+    validators: {
+      onSubmit: mealFormSchema,
+    },
+    defaultValues,
+    async onSubmit({ value, formApi }) {
+      const tx = mealCollection.insert({
+        ...value,
+        datetime: value.datetime.toString().replace('T', ' '),
+      })
+      await tx.isPersisted.promise
+      formApi.reset()
+      close()
+    },
+  })
+
+  return (
+    <>
+      <Sheet.Header>
+        <Sheet.Title>Create Meal</Sheet.Title>
+      </Sheet.Header>
+      <TanstackForm form={form} AppForm={form.AppForm}>
+        <Sheet.Body className="grid gap-4">
+          <form.ServerErrorNote />
+
+          <FieldGroupMeal
+            form={form}
+            fields={{
+              id: 'id',
+              datetime: 'datetime',
+              type: 'type',
+              items: 'items',
+            }}
+          />
+
+          <Separator />
+        </Sheet.Body>
+        <Sheet.Footer>
+          <form.SubscribeButton>Create</form.SubscribeButton>
+        </Sheet.Footer>
+      </TanstackForm>
+    </>
+  )
+}
+
+export function UpdateMealSheetContent({
+  meal,
+  close,
+}: {
+  meal: Meal
+  close: () => void
+}) {
+  const defaultValues: z.infer<typeof mealFormSchema> = useMemo(
+    () => ({
+      ...meal,
+      datetime: parseDateTime(meal.datetime.replace(' ', 'T')),
+    }),
+    [meal],
+  )
+
+  const form = useAppForm({
+    validators: {
+      onSubmit: mealFormSchema,
+    },
+    defaultValues,
+    async onSubmit({ value, formApi }) {
+      const tx = mealCollection.update(meal.id, (draft) => {
+        draft.datetime = value.datetime.toString().replace('T', ' ')
+        draft.items = value.items.filter((item) => item.trim() !== '')
+        draft.type = value.type
+      })
+      await tx.isPersisted.promise
+      formApi.reset()
+      close()
+    },
+  })
+
+  return (
+    <>
+      <Sheet.Header>
+        <Sheet.Title>Meal</Sheet.Title>
+      </Sheet.Header>
+      <TanstackForm form={form} AppForm={form.AppForm}>
+        <Sheet.Body className="grid gap-4">
+          <form.ServerErrorNote />
+
+          <FieldGroupMeal
+            form={form}
+            fields={{
+              id: 'id',
+              datetime: 'datetime',
+              type: 'type',
+              items: 'items',
+            }}
+          />
+
+          <Separator />
+        </Sheet.Body>
+        <Sheet.Footer>
+          <form.SubscribeButton />
+        </Sheet.Footer>
+      </TanstackForm>
+    </>
+  )
+}
+
+const { label } = fieldStyles()
+
+const mealFormSchema = mealSchema.extend({
+  datetime: z.instanceof(CalendarDateTime),
+})
+
+const FieldGroupMeal = withFieldGroup({
+  defaultValues: {} as z.infer<typeof mealFormSchema>,
+  render: function Render({ group }) {
+    return (
+      <>
+        <group.AppField name="datetime">
+          {(field) => <field.DatePicker label="Date and Time" />}
+        </group.AppField>
+
+        <group.AppField name="type">
+          {(field) => (
+            <field.SelectField label="Type">
+              <Select.Trigger />
+              <Select.Content
+                items={mealTypeSchema.options.map((id) => ({ id }))}
+              >
+                {(item) => (
+                  <Select.Item
+                    id={item.id}
+                    textValue={mealTypeToDisplayText[item.id]}
+                  >
+                    {mealTypeToDisplayText[item.id]}
+                  </Select.Item>
+                )}
+              </Select.Content>
+            </field.SelectField>
+          )}
+        </group.AppField>
+
+        <group.Field name="items" mode="array">
+          {(field) => {
+            return (
+              <div className="flex flex-col gap-y-1">
+                <h3 className={label()}>Items</h3>
+                {field.state.value.map((_, i) => {
+                  return (
+                    <group.AppField key={i} name={`items[${i}]`}>
+                      {(subField) => {
+                        return (
+                          <div>
+                            <subField.TextField
+                              suffix={
+                                <Button
+                                  size="sq-xs"
+                                  aria-label="New user"
+                                  onPress={() => field.removeValue(i)}
+                                  intent="plain"
+                                >
+                                  <IconTrash />
+                                </Button>
+                              }
+                            />
+                          </div>
+                        )
+                      }}
+                    </group.AppField>
+                  )
+                })}
+                <div className="pt-1">
+                  <Button
+                    onPress={() => field.pushValue('')}
+                    intent="secondary"
+                    type="button"
+                    size="sm"
+                  >
+                    Add item
+                  </Button>
+                </div>
+              </div>
+            )
+          }}
+        </group.Field>
+      </>
+    )
+  },
+})
---
+++ src/routes/calendar.tsx
@@ -0,0 +1,246 @@
+import {
+  GridList,
+  GridListEmptyState,
+  GridListItem,
+} from '@/components/ui/grid-list'
+import { mealCollection } from '@/db-collections'
+import { cn } from '@/lib/utils'
+import { Meal, mealTypeToDisplayText } from '@/schemas/meal'
+import { eq, like, useLiveQuery } from '@tanstack/react-db'
+import { createFileRoute, createLink } from '@tanstack/react-router'
+import { useMemo } from 'react'
+import {
+  GridListItemProps,
+  isTextDropItem,
+  Link,
+  useDragAndDrop,
+} from 'react-aria-components'
+import { Temporal } from 'temporal-polyfill'
+import { Interval, startOfWeek } from 'vremel'
+import {
+  CreateMealSheetContent,
+  UpdateMealSheetContent,
+  validateSearch,
+} from './-shared'
+import {
+  useIsTransribeMutationMutating,
+  VoiceRecorder,
+} from './-voice-recorder'
+import { Button, buttonStyles } from '@/components/ui/button'
+import { Sheet } from '@/components/ui/sheet'
+import { IconVoice } from '@intentui/icons'
+
+export const Route = createFileRoute('/calendar')({
+  component: RouteComponent,
+  ssr: false,
+  validateSearch: validateSearch,
+})
+
+const TanstackLink = createLink(Link)
+
+function RouteComponent() {
+  const days: DayType[] = useMemo(() => {
+    const today = Temporal.Now.plainDateISO()
+    const weekStart = startOfWeek(today, { firstDayOfWeek: 1 })
+    const weekInterval = {
+      start: weekStart,
+      end: weekStart.add({ weeks: 1 }),
+    }
+
+    const days = getDaysInInterval(weekInterval).map((day) => ({
+      date: day,
+      isToday: day.equals(today),
+    }))
+
+    return days
+  }, [])
+
+  const navigate = Route.useNavigate()
+  const search = Route.useSearch()
+  const selectedMeal = useLiveQuery(
+    (q) =>
+      q
+        .from({ meal: mealCollection })
+        .where(({ meal }) => eq(meal.id, search.meal))
+        .findOne(),
+    [search.meal],
+  )
+  const isTransribeMutationMutating = useIsTransribeMutationMutating()
+
+  return (
+    <div>
+      <div className="flex gap-2">
+        <VoiceRecorder>
+          <Button
+            size="sq-md"
+            isCircle
+            isDisabled={isTransribeMutationMutating > 0}
+          >
+            <IconVoice />
+          </Button>
+        </VoiceRecorder>
+        <TanstackLink
+          from={Route.fullPath}
+          search={{ add: true }}
+          className={buttonStyles({ intent: 'secondary' })}
+        >
+          Create meal
+        </TanstackLink>
+      </div>
+
+      <Sheet
+        isOpen={search.add !== undefined}
+        onOpenChange={() => navigate({ search: { meal: undefined } })}
+      >
+        <Sheet.Content>
+          {({ close }) => <CreateMealSheetContent close={close} />}
+        </Sheet.Content>
+      </Sheet>
+
+      <Sheet
+        isOpen={search.meal !== undefined}
+        onOpenChange={() => navigate({ search: { add: undefined } })}
+      >
+        <Sheet.Content>
+          {({ close }) =>
+            selectedMeal.data ? (
+              <UpdateMealSheetContent meal={selectedMeal.data} close={close} />
+            ) : null
+          }
+        </Sheet.Content>
+      </Sheet>
+
+      <div className="grid auto-cols-[200px] grid-flow-col gap-2 overflow-x-auto divide-x">
+        {days.map((day) => (
+          <Day day={day} key={day.date.toString()} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+type DayType = {
+  date: Temporal.PlainDate
+  isToday: boolean
+}
+
+function Day({ day }: { day: DayType }) {
+  const meals = useLiveQuery((q) =>
+    q
+      .from({ meal: mealCollection })
+      .where(({ meal }) => like(meal.datetime, `${day.date.toString()}%`))
+      .orderBy(({ meal }) => meal.datetime, 'asc'),
+  )
+
+  const { dragAndDropHooks } = useDragAndDrop<Meal>({
+    // Accept drops with the custom format.
+    acceptedDragTypes: ['custom-app-type'],
+
+    // Ensure items are always moved rather than copied.
+    getDropOperation: () => 'move',
+
+    getItems(_keys, items) {
+      return items.map((item) => {
+        return {
+          'custom-app-type': item.id,
+        }
+      })
+    },
+
+    async onInsert(e) {
+      const processedItems = await Promise.all(
+        e.items
+          .filter(isTextDropItem)
+          .map(async (item) => await item.getText('custom-app-type')),
+      )
+      mealCollection.update(processedItems, (drafts) => {
+        drafts.forEach((draft) => {
+          const time = Temporal.PlainDateTime.from(draft.datetime).toPlainTime()
+          draft.datetime = day.date
+            .toPlainDateTime(time)
+            .toString()
+            .replace('T', ' ')
+        })
+      })
+    },
+
+    async onRootDrop(e) {
+      const processedItems = await Promise.all(
+        e.items
+          .filter(isTextDropItem)
+          .map(async (item) => await item.getText('custom-app-type')),
+      )
+      mealCollection.update(processedItems, (drafts) => {
+        drafts.forEach((draft) => {
+          const time = Temporal.PlainDateTime.from(draft.datetime).toPlainTime()
+          draft.datetime = day.date.toPlainDateTime(time).toString()
+        })
+      })
+    },
+  })
+
+  return (
+    <div>
+      <div className={cn('p-2 text-center', !day.isToday && 'text-muted-fg')}>
+        {day.date.toLocaleString('en-US', {
+          weekday: 'short',
+          day: 'numeric',
+        })}
+      </div>
+
+      <GridList
+        aria-label={`Meal List ${day.date.toLocaleString('en-US', { weekday: 'long' })}`}
+        className="flex flex-col gap-2 p-2"
+        items={meals.data}
+        dragAndDropHooks={dragAndDropHooks}
+        renderEmptyState={() => (
+          <GridListEmptyState className="text-center text-muted-fg text-sm">
+            No meals
+          </GridListEmptyState>
+        )}
+      >
+        {(item) => <MealCard key={item.id} meal={item} />}
+      </GridList>
+    </div>
+  )
+}
+
+function MealCard({ meal, ...props }: { meal: Meal } & GridListItemProps) {
+  const date = Temporal.PlainDateTime.from(meal.datetime)
+  return (
+    <GridListItemLink
+      from={Route.fullPath}
+      search={{ meal: meal.id }}
+      className="p-2 flex-col gap-2 bg-white"
+      aria-label={`Meal ${meal.id}`}
+      textValue={`Meal ${meal.id}`}
+      key={meal.id}
+      {...props}
+    >
+      <ul className="list-disc pl-4 text-sm">
+        {meal.items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+
+      <div className="text-muted-fg text-sm flex gap-1">
+        <div>{mealTypeToDisplayText[meal.type]}</div> ·
+        <div>{date.toLocaleString('en-US', { timeStyle: 'short' })}</div>
+      </div>
+    </GridListItemLink>
+  )
+}
+
+const GridListItemLink = createLink(GridListItem)
+
+function getDaysInInterval(interval: Interval<Temporal.PlainDate>) {
+  const days = []
+  for (
+    let date = interval.start;
+    Temporal.PlainDate.compare(date, interval.end) < 0;
+    date = date.add({ days: 1 })
+  ) {
+    days.push(date)
+  }
+  return days
+}
